fd8ce52cce496f109d4fee87160e5ff8bb8df8df
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2020-11-30 10:32:37 +0530

    set up express gen

--- mind-maps/src/index.js
+++ mind-maps/src/index.js
@@ -22,17 +22,42 @@ function MindMap(){
         setKey(prevKey => prevKey + 1);
     }
 
-    
+    function handleSubmit () {
+        
+    }
 
     return (<div>
         <MindMapArea elems={notes}/>
         <br />
-        <Button color="green" text="Add Node" handleClick={handleclick}/>
+        <Button color="green" text="Add Node" style="position:relative" handleClick={handleclick}/>
+        <Button color="green" text="Save" style="position:relative" handleClick={handleSubmit} />
     </div>);
 }
 
-function App(){
-    return (<MindMap />);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={apiResponse:""};
+    }
+
+    callAPI() {
+        fetch("http://localhost:8000/test-api")
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}));
+    }
+
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render(){
+        return (
+            <div>
+                <MindMap />
+                <p>{this.state.apiResponse}</p>
+            </div>
+        );
+    }
 }
 
 document.querySelector('html').style.scrollBehavior = "smooth";
