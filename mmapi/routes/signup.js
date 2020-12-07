1071bce1651640078504a5b42edb785ad1731ba3
Divya Shekar <divyashekar@MacBook-Pro.local>
2020-12-07 14:38:45 +0530

    Added form methods

---
+++ mmapi/routes/signup.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+router.post('/', function(req, res){
+  MongoClient.connect('mongodb://localhost:27017', {
+      useUnifiedTopology:true
+  }, 
+  
+  function(err,client){
+      if(err) throw err;
+
+      var name = req.body.name; 
+      var email =req.body.email; 
+    
+      var data = { 
+          "name": name, 
+          "email":email, 
+      } 
+
+      const db = client.db('newdb'); 
+      db.collection('signup').insertOne(data,function(err, collection){ 
+          if(err) throw err;
+
+          console.log("[INFO] POST successful.");
+          client.close();
+      });
+  });
+})
+
+module.exports = router;
