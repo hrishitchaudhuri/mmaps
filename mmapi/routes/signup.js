fca96589cef0d78aeb91bbdcfcb74a22fb753291
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2020-12-09 09:50:06 +0530

    Added mailing list

--- mmapi/routes/signup.js
+++ mmapi/routes/signup.js
@@ -1,10 +1,82 @@
+var nodemailer = require('nodemailer');
 var express = require('express');
 var router = express.Router();
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
+router.get('/', function(req, res) {
+    res.writeHead(200, {'Content-type':'application/json'});
+    MongoClient.connect('mongodb://localhost:27017', {
+      useUnifiedTopology:true
+  }, 
+
+  function(err, client) {
+      if (err) throw err;
+
+      const db = client.db('newdb');
+      db.collection('signup').find({}).toArray(function(err, objs) {
+          if (err) throw err;
+
+          res.write(JSON.stringify(objs));
+          console.log(objs);
+
+          res.end();
+          client.close();
+      })
+  })
+});
+
+/*
+router.post('/mailToAll', function(req, res) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'mindmaps94@gmail.com',
+          pass: 'mindMAPS-98'
+        }
+    });
+
+    MongoClient.connect('mongodb://localhost:27017', {
+        useUnifiedTopology:true
+    }, 
+    
+    
+    function(err, client) {
+        const db = client.db('newdb');
+        db.collection('signup').find({}).toArray(function(err, objs) {
+            if (err) throw err;
+
+            for (i = 0; i < objs.length; i++) {
+                var mailOptions = {
+                    from: 'mindmaps94@gmail.com',
+                    to: objs[i].email,
+                    subject: 'Sending Email using Node.js',
+                    text: 'Hello' + toString(objs[i].name)
+                };
+
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log('Email sent: ' + info.response);
+                    }
+                })
+            }
+        })
+    })
+})
+*/
+
 router.post('/', function(req, res){
-  MongoClient.connect('mongodb://localhost:27017', {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'mindmaps94@gmail.com',
+          pass: 'mindMAPS-98'
+        }
+    });
+  
+    MongoClient.connect('mongodb://localhost:27017', {
       useUnifiedTopology:true
   }, 
   
@@ -18,6 +90,21 @@ router.post('/', function(req, res){
           "name": name, 
           "email":email, 
       } 
+
+      var mailOptions = {
+        from: 'mindmaps94@gmail.com',
+        to: email,
+        subject: 'Hello!',
+        text: 'Hello, Welcome to Mind Maps!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
 
       const db = client.db('newdb'); 
       db.collection('signup').insertOne(data,function(err, collection){ 
